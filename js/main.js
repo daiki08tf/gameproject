5b934c5db33c59c2f0afa294fa26fdf3d6fdc897
daiki08tf <93368145+daiki08tf@users.noreply.github.com>
2026-08-23 21:36:19 +0900

    feat: load Codex 2.0 foundation

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ import './patches/weaponInstanceFoundation.js';
 import './patches/companionFoundation.js';
 import './patches/companionBattle.js';
 import './patches/companionRecruitment.js';
+import './patches/codexFoundation.js';
 import './patches/homeNavigation.js';
 import './patches/rune2Ui.js';
 import './patches/rune2ObserveUi.js';
