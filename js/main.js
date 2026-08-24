10414b63bdd3d4bd86df5cb4876506d7b7c39460
daiki08tf <93368145+daiki08tf@users.noreply.github.com>
2026-08-24 11:57:20 +0900

    Load Job 3.0 inherited MASTER passives

--- js/main.js
+++ js/main.js
@@ -32,6 +32,7 @@ import './patches/combat2SkillModifiers.js';
 import './patches/combat2SkillModifierUi.js';
 import './patches/combat2DebugSafety.js';
 import './patches/job3SpecializationCore.js';
+import './patches/job3LegacyPassives.js';
 import './patches/companionFoundation.js';
 import './patches/companionResetSafety.js';
 import './patches/companionEvolution.js';
