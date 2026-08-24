1632c7a0d1d97ce034a1cc159815c40f39015cd4
daiki08tf <93368145+daiki08tf@users.noreply.github.com>
2026-08-24 11:52:19 +0900

    Load Job 3.0 specialization runtime

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ import './patches/combat2WeaponTechniques.js';
 import './patches/combat2SkillModifiers.js';
 import './patches/combat2SkillModifierUi.js';
 import './patches/combat2DebugSafety.js';
+import './patches/job3SpecializationCore.js';
 import './patches/companionFoundation.js';
 import './patches/companionResetSafety.js';
 import './patches/companionEvolution.js';
