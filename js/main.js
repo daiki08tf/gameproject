8e32da6f314d03c4d729294c800ee1fa2713f08c
daiki08tf <93368145+daiki08tf@users.noreply.github.com>
2026-08-24 11:17:18 +0900

    feat: load Equipment 3.0 set bonus runtime

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ import './patches/equipment3Legendary.js';
 import './patches/equipment3Blacksmith.js';
 import './patches/equipment3GearFoundation.js';
 import './patches/equipment3UniqueGearSafety.js';
+import './patches/equipment3SetBonuses.js';
 import './patches/equipment3SmartLoot.js';
 import './patches/equipment3AbyssEndgame.js';
 import './patches/equipment3DebugSafety.js';
