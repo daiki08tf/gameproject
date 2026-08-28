ecaedb748844d7a68f36e00fba61a2cb1d8c8846
daiki08tf <93368145+daiki08tf@users.noreply.github.com>
2026-08-28 20:50:50 +0900

    feat(enemy2): load E8 story encounter migration

--- js/patches/battle2RoadmapComplete.js
+++ js/patches/battle2RoadmapComplete.js
@@ -4,6 +4,7 @@ import './enemy2LevelScaling.js';
 import './enemy2EncounterPilot.js';
 import './enemy2EncounterTemplates.js';
 import './enemy2RankVariants.js';
+import './enemy2StoryMigration.js';
 import { BattleEngine } from '../battleEngine.js';
 import { TextBattleScreen } from '../screens/textBattle.js';
 import { BREAK_RULES,COMPANION_TACTICS,breakCapacity,breakDamage,comboForHit,tickCombatStatuses } from '../data/battle2Tactics.js';
