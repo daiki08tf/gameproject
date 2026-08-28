3e5bd85dda2d7dc0d63058aaf04fe8db6017fc17
daiki08tf <93368145+daiki08tf@users.noreply.github.com>
2026-08-29 00:10:49 +0900

    Enemy 3.0 B7: load Boss phase AI

--- js/patches/enemy3Targeting.js
+++ js/patches/enemy3Targeting.js
@@ -2,6 +2,7 @@
 import './enemy3EliteAffixes.js';
 import './enemy3RareBehaviors.js';
 import './enemy3EncounterSynergy.js';
+import './enemy3BossPhaseAI.js';
 import { BattleEngine } from '../battleEngine.js';
 import { enemy3RoleBehaviorProfile } from '../data/enemy3RoleBehaviors.js';
 import { enemy3TacticalContext,enemy3ShouldReserveSkill } from '../data/enemy3Targeting.js';
