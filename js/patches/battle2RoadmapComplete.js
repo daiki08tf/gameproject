4bce3ff0f977c624fb4ed7ac6d862ae608fdd4c3
daiki08tf <93368145+daiki08tf@users.noreply.github.com>
2026-08-28 20:10:52 +0900

    Enemy 2.0 E5: load encounter pilot after level scaling

--- js/patches/battle2RoadmapComplete.js
+++ js/patches/battle2RoadmapComplete.js
@@ -1,6 +1,7 @@
 /* Battle 2.0 roadmap completion: Break, combos, companion tactics */
 import './enemy2LevelFoundation.js';
 import './enemy2LevelScaling.js';
+import './enemy2EncounterPilot.js';
 import { BattleEngine } from '../battleEngine.js';
 import { TextBattleScreen } from '../screens/textBattle.js';
 import { BREAK_RULES,COMPANION_TACTICS,breakCapacity,breakDamage,comboForHit,tickCombatStatuses } from '../data/battle2Tactics.js';
