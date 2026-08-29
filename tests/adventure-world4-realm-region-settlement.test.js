54b09e2f68a4baee551188b387fec47c95d01fab
daiki08tf <93368145+daiki08tf@users.noreply.github.com>
2026-08-30 02:13:49 +0900

    test: confirm integration import is regression source

--- tests/adventure-world4-realm-region-settlement.test.js
+++ tests/adventure-world4-realm-region-settlement.test.js
@@ -1,5 +1,3 @@
 import test from 'node:test';
-import '../js/patches/adventureWorld4RealmRegionRuntime.js';
-import '../js/patches/adventureWorld4SceneRuntime.js';
 
-test.skip('diagnostic: Realm integration assertions temporarily isolated',()=>{});
+test.skip('diagnostic: Realm runtime imports temporarily isolated',()=>{});
